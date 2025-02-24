9846af496d59dc3dc93ee8be130e287283e30c23
Yash vardhan Singh <yashvardhansinghparihar10@gmail.com>
2025-02-24 15:13:11 +0530

    Created A Helloworld Spring app

---
+++ helloworldspringapp/src/main/java/com/example/helloworldspringapp/HelloworldSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.helloworldspringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldSpringAppApplication.class, args);
+	}
+
+}
